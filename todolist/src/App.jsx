941cb738e0774d17f113351100f2f040f9b75690
felipeffranco <felipeffranco@outlook.com>
2023-10-23 22:56:04 -0300

    start css

---
+++ todolist/src/App.jsx
@@ -0,0 +1,15 @@
+import './global.css'
+
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <h1>Teste</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
